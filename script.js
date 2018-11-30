f8ed25443914d40a9ebd12001dcd90c1eca14a0a
marwales <marcin.regent@gmail.com>
2018-11-30 10:51:26 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function decToBin() {
+  var num = document.getElementById('userInput').value;
+  var result = [];
+ while (num >= 1) {
+   result.unshift(num % 2);
+   num = Math.floor(num / 2);
+   
+    
+ }
+
+ document.getElementById('conversion').innerHTML = result;
+}
+  
+
