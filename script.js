67eb6149bada3ed07194dd0cff6485fc02ff40e4
marwales <marcin.regent@gmail.com>
2018-12-01 19:21:38 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function decToBin() {
   var num = document.getElementById('userInput').value;
   var result = [];
+
+if (num == 0) {
+  result = [0];
+}
+else if (num > 0){
  while (num >= 1) {
    result.unshift(num % 2);
    num = Math.floor(num / 2);
-
-
  }
+}
+else {
+  result = ['Please input positive number only']
+}
 
  document.getElementById('conversion').innerHTML = result.join('');
 }
